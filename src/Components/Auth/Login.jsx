1b64c42aedc5a4bbf878cd3641f3e1dd26b26862
unknown <tamiloreolaniyi@gmail.com>
2025-07-09 07:33:14 +0100

    push

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -114,18 +114,7 @@ export default function SignIn() {
             </div>
 
             {/* Remember Me & Forgot Password */}
-            <div className="flex items-center justify-between">
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  checked={rememberMe}
-                  onChange={(e) => setRememberMe(e.target.checked)}
-                  className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <span className="ml-2 text-sm text-gray-600">
-                  Remember for 30 days
-                </span>
-              </label>
+            <div className="flex items-center justify-end">
               <button
                 type="button"
                 onClick={() => console.log("Forgot password clicked")}
